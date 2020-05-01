4136711a7ea30062a975986c67b776cfb4c2da61
pockman <pockman@163.com>
2020-05-01 06:24:58 +0000

    add func ready.

--- hello.c
+++ hello.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
 
+
+int  add(int a, int b) {
+    return a + b;
+}
+
 int main(int argc, char* argv[]) {
     printf("hello.\n");
+    printf("%d\n", add(5,4));
 
     return 0;
 }
